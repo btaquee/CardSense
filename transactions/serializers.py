3f69dbe1595af604128f71c3436fa7bb71f25a7b
XiyuanWu <xiyuan.24706826@gmail.com>
2025-10-27 23:17:20 -0700

    tran

---
+++ transactions/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Transaction
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ("id", "user", "card", "merchant", "amount", "category", "created_at", "updated_at", "notes")
+        read_only_fields = ("user", "created_at", "updated_at")
--- transactions/urls.py
+++ transactions/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import TransactionViewSet
+
+router = DefaultRouter()
+router.register(r'transactions', TransactionViewSet, basename='transactions')
 
 urlpatterns = [
     path('health/', views.HealthCheckView.as_view(), name='health'),
